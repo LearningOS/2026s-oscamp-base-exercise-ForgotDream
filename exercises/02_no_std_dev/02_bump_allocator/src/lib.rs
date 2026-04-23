1cbdbfbf44add1ba39998c4c9cd2c03b49ad73af
ForgotDream <mr.zyz@outlook.com>
2026-04-23 11:47:22 +0800

    0202

--- exercises/02_no_std_dev/02_bump_allocator/src/lib.rs
+++ exercises/02_no_std_dev/02_bump_allocator/src/lib.rs
@@ -74,7 +74,36 @@ unsafe impl GlobalAlloc for BumpAllocator {
         // 5. Atomically update next to end using compare_exchange
         //    (if CAS fails, another thread raced — retry in a loop)
         // 6. Return the aligned address as a pointer
-        todo!()
+
+        let mut current_next = self.next.load(Ordering::SeqCst);
+
+        loop {
+            let align = layout.align();
+            let aligned = (current_next + align - 1) & !(align - 1);
+
+            let alloc_end = match aligned.checked_add(layout.size()) {
+                Some(end) => end,
+                None => return null_mut()
+            };
+
+            if alloc_end > self.heap_end {
+                return null_mut();
+            }
+
+            match self.next.compare_exchange(
+                current_next, 
+                alloc_end, 
+                Ordering::SeqCst, 
+                Ordering::SeqCst
+            ) {
+                Ok(_) => {
+                    return aligned as *mut u8;
+                },
+                Err(new_next) => {
+                    current_next = new_next;
+                }
+            }
+        }
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
